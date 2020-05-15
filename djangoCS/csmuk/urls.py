a0b93e1f643ac97c1ac75e0fb7e4da6fc99eccf7
Noppanut-Wapee <machanics.02@gmail.com>
2020-05-15 12:34:15 +0700

    csmuk

--- djangoCS/csmuk/urls.py
+++ djangoCS/csmuk/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import HomePage
+from .views import HomePage, AboutPage, ContactUs  #ดึงฟังชัน์ใน csmuk มาใช้งาน
+
 urlpatterns = [  
     # localhost:8000/
-    path('',HomePage),
+    path('',HomePage, name='home-page'),
+    path('about/',AboutPage, name='about-page'),
+    path('contact/',ContactUs, name='contact-page'),
+
 ]
--- djangoCS/csmuk/views.py
+++ djangoCS/csmuk/views.py
@@ -5,3 +5,10 @@ def HomePage(request):
     #return HttpResponse('<h1> Hello World Pun</h1>')
     return render(request,'csmuk/home.html')
 # Create your views here.
+
+
+def AboutPage(request):
+    return render(request,'csmuk/about.html')
+
+def ContactUs(request):
+    return render(request,'csmuk/contactus.html')
