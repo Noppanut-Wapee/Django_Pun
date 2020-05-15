79117d6358919253769aed6a3e787f77432f39e0
Noppanut-Wapee <machanics.02@gmail.com>
2020-05-15 16:30:59 +0700

    csmuk

--- djangoCS/csmuk/models.py
+++ djangoCS/csmuk/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    allsubject = (('City','ในเมือง'),
+                  ('Suburb','นอกเขตเมือง'))
+
+
+    customer_subject = models.CharField(max_length=200, choices=allsubject, default='city')
+    customer_name = models.CharField(max_length=200)
+    customer_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.customer_name + ' - ' + self.customer_subject+ ' - ' + str(self.customer_id)
+    
+
+
