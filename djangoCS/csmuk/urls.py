47ef7f4115e33836f05a5ffb37cdc0682a617fb9
Noppanut-Wapee <machanics.02@gmail.com>
2020-05-15 17:16:14 +0700

    csmuk

--- djangoCS/csmuk/urls.py
+++ djangoCS/csmuk/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import HomePage, AboutPage, ContactUs  #ดึงฟังชัน์ใน csmuk มาใช้งาน
-
+# from .views import HomePage, AboutPage, ContactUs, Showcustomer  #ดึงฟังชัน์ใน csmuk มาใช้งาน
+from .views import * #แบบนี้ไม่ต้องพิมพ์ จะเอามาทั้งหมดเลย
 urlpatterns = [  
     # localhost:8000/
     path('',HomePage, name='home-page'),
     path('about/',AboutPage, name='about-page'),
     path('contact/',ContactUs, name='contact-page'),
+    path('showcustomer/',Showcustomer, name='customer-page'),
 
 ]
--- djangoCS/csmuk/views.py
+++ djangoCS/csmuk/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render # ดึงมาจาก  template
 from django.http import HttpResponse #เขียนเอง
+from .models import Customer
 
 def HomePage(request):
     #return HttpResponse('<h1> Hello World Pun</h1>')
@@ -12,3 +13,8 @@ def AboutPage(request):
 
 def ContactUs(request):
     return render(request,'csmuk/contactus.html')
+
+def Showcustomer(request):
+    customer_all = Customer.objects.all() #ดึงค่าจาก database ทั้งหมด
+    context = {'customer_all':customer_all}
+    return render(request,'csmuk/showcustomer.html', context)
